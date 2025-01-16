eaf639267f70ad211cc45c5d1df168f3a37fc8e1
jadewisemann <jadewisemann@gmail.com>
2025-01-16 17:33:39 +0900

    기능: 추가, 검색 결과 페이지 웹-컴포넌트 랜더링

---
+++ src/pages/search-result.js
@@ -0,0 +1,25 @@
+
+import "/src/components/movie-card.js"
+// import "../components/movie-card.js"
+
+import { getSearchResult } from "/src/utils/fetch.js"
+
+const urlParams = new URLSearchParams(window.location.search);
+const title = urlParams.get('title');
+
+const searchResult = document.querySelector('.search-result');
+
+(async (title) => {
+  const movieDetail = await getSearchResult(title);
+  Object.values(movieDetail).forEach(movie => {
+    const movieCardElement = document.createElement('movie-card');
+    movieCardElement.setAttribute('title', movie.Title);
+    movieCardElement.setAttribute('year', movie.Year);
+    movieCardElement.setAttribute('imdb-id', movie.ImdbID);
+    movieCardElement.setAttribute('type', movie.Type);
+    movieCardElement.setAttribute('poster', movie.Poster);
+    searchResult.appendChild(movieCardElement);
+  })
+})(title)
+
+
