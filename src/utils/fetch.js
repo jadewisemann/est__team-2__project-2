1caed66f0770a933120b4abeede4d8b5188d05a5
jadewisemann <jadewisemann@gmail.com>
2025-01-20 00:52:45 +0900

    feat: add 'getMoveiDetailHiRes' function

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,5 +1,6 @@
-const API_URL = 'http://www.omdbapi.com/?apikey=33c97183'
-
+const API_KEY = '33c97183'
+const API_URL = 'http://www.omdbapi.com'
+const API_POSTER_URL = `http://www.img.omdbapi.com`
 const fetchAPI = async (url) => {
   try {
     const response = await fetch(url)
@@ -15,16 +16,32 @@ const fetchAPI = async (url) => {
 }
 
 export const getSearchResult = async (title, year=``, page=1) => {
-  const data = await fetchAPI(`${API_URL}&s=${title}&y=${year}&page=${page}`);
+  const data = await fetchAPI(`${API_URL}?apikey=${API_KEY}&s=${title}&y=${year}&page=${page}`);
   return {...data.Search}
 }
 
 export const getSearchResultMore = async (title, year=``, page=1) => {
-  const data = await fetchAPI(`${API_URL}&s=${title}&y=${year}&page=${page}`);
+  const data = await fetchAPI(`${API_URL}?apikey=${API_KEY}&s=${title}&y=${year}&page=${page}`);
   return {...data.Search}
 }
 
 
 export const getMovieDetail = async (imdbID) => {
-  return await fetchAPI(`${API_URL}&i=${imdbID}`);
+  return await fetchAPI(`${API_URL}?apikey=${API_KEY}&i=${imdbID}`);
+}
+
+
+export const getMovieDetailHiRes = async (imdbID) => {
+  const movieDetails = await getMovieDetail(imdbID)
+  try {
+    const poster = await fetchAPI(`${API_POSTER_URL}?apikey=${API_KEY}&i=${imdbID}`)
+    if (!poster.ok) {
+      throw new Error(`can't fetch high-res poster`)
+    }
+    movieDetails.Poster = poster
+    return movieDetails          
+  } catch (error) {
+    console.error(error)
+    return movieDetails
+  }
 }
