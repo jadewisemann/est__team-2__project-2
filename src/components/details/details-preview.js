2853c14d34a4055ea10949d9a270a1ebc16bd0d4
choroc <a1004712@naver.com>
2025-01-23 15:21:37 +0900

    test: new branch

--- src/components/details/details-preview.js
+++ src/components/details/details-preview.js
@@ -1,16 +1,34 @@
 class DetailsPreview extends HTMLElement{
     constructor(){
         super()
+        this.imdb_id = null;
     }
     connectedCallback(){
-        this.render()
+        // this.render()
+    }
+    async setImdbId(id){
+        this.imdb_id = id
+        await this.render
     }
 
-    async getData(){
-        const response = fetch()
+    async getOmdbApiData() {
+        const API_URL = 'http://www.omdbapi.com/?apikey=33c97183'
+        try {
+            const response = await fetch(`${API_URL}&i=${this.imdb_id}`)
+            if(!response.ok){
+                throw new Error("API 호출 실패")
+            }
+            return response.json()
+        } catch (error) {
+            console.error('문제발생',error);
+        }
     }
+    
 
-    render(){
+    async render(){
+        const dataOmdb = await this.getOmdbApiData()
+
+
 
     }
 }
