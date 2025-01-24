9122cfe2fe3d0bfe5f5089da2b1f4c835f30879e
jadewisemann <jadewisemann@gmail.com>
2025-01-24 09:23:01 +0900

    test:

--- @test/comp-btn.js
+++
@@ -1,7 +0,0 @@
-const date = async () => {}
-const target = document.querySelector('.comp-btn')
-target.innerHTML = `
-  <div>
-  hello world
-  </div> 
-`
--- @test/components/index.js
+++
@@ -1,4 +0,0 @@
-import './test-comp.js'
-import './test-comp1.js'
-import './test-comp2.js'
-
