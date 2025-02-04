df4f2579cb8dc58d652a8b993a7ba9da4c5b479e
jadewisemann <jadewisemann@gmail.com>
2025-02-04 13:16:20 +0900

    feat: add loading animation

--- src/pages/search-result.js
+++ src/pages/search-result.js
@@ -4,6 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
   initMovieLoader();
 });
 
+
+
+const loadingAnimation = /*html*/`
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><rect fill="#3887FF" stroke="#3887FF" stroke-width="8" width="30" height="30" x="25" y="85"><animate attributeName="opacity" calcMode="spline" dur="3" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate></rect><rect fill="#3887FF" stroke="#3887FF" stroke-width="8" width="30" height="30" x="85" y="85"><animate attributeName="opacity" calcMode="spline" dur="3" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate></rect><rect fill="#3887FF" stroke="#3887FF" stroke-width="8" width="30" height="30" x="145" y="85"><animate attributeName="opacity" calcMode="spline" dur="3" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate></rect></svg>
+`
+
 const loadMore = document.querySelector('.load-more');
 
 let currentPage = 1;
@@ -30,9 +36,9 @@ function getQueryParam(param) {
 const showLoading = (container) => {
   const loadingEl = document.createElement("div");
   loadingEl.id = "loading-indicator";
-  loadingEl.textContent = "Loading...";
+  loadingEl.innerHTML = `<p>${loadingAnimation}</p>`
   container.appendChild(loadingEl);
-};
+}
 
 const hideLoading = () => {
   const loadingEl = document.getElementById("loading-indicator");
@@ -62,8 +68,8 @@ function initMovieLoader() {
 async function fetchMovieDataByGenre(genreId, page = 1, movieContainer, textSearch) {
   try {
     if (page === 1) {
-      movieContainer.innerHTML = "";
-      showLoading(movieContainer);
+      movieContainer.innerHTML = ""
+      showLoading(movieContainer)
     }
     const response = await fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&with_genres=${genreId}&language=en-US&page=${page}`,
@@ -72,13 +78,13 @@ async function fetchMovieDataByGenre(genreId, page = 1, movieContainer, textSear
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
     const data = await response.json();
-    hideLoading();
+    hideLoading()
 
     currentMovies = page === 1 ? data.results : [...currentMovies, ...data.results];
     displayMoviesFromTmdb(data.results, movieContainer, textSearch);
   } catch (error) {
-    hideLoading();
-    console.error("Error fetching movie data:", error);
+    hideLoading()
+    console.error("Error fetching movie data:", error)
     movieContainer.innerHTML = '<h2 class="text--search">Movie Not Found</h2>';
   }
 }
